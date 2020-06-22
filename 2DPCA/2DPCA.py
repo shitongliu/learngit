70d393e62b52abd584d04b84771f2e22f69b1edc
shitongliu <52740485+shitongliu@users.noreply.github.com>
2020-06-22 14:26:26 +0800

    Add files via upload

--- 2DPCA/2DPCA.py
+++ 2DPCA/2DPCA.py
@@ -64,8 +64,9 @@ def pca_2d(imgs, n_component):
 
 
 print('start.')
-d1, d2, d3 = 10, 30, 60
+d1, d2, d3 = 10, 30, 60     # number of components
 all_imgs = read_images_from_disk('ORL')
+
 all_V1, U1 = pca_2d(all_imgs, d1)
 all_V2, U2 = pca_2d(all_imgs, d2)
 all_V3, U3 = pca_2d(all_imgs, d3)
@@ -81,30 +82,86 @@ A_cap3 = all_V3[no] @ U3.T
 
 # display the result
 plt.figure()
-plt.subplot(2, 2, 1)
+plt.subplot(1, 4, 1)
 plt.imshow(A, cmap='gray')
 plt.xticks([])
 plt.yticks([])
 plt.xlabel('original image')
 
-plt.subplot(2, 2, 2)
+plt.subplot(1, 4, 2)
 plt.imshow(A_cap1, cmap='gray')
 plt.xticks([])
 plt.yticks([])
-plt.xlabel('reconstructed image(d={})'.format(d1))
+plt.xlabel('d={}'.format(d1))
 
-plt.subplot(2, 2, 3)
+plt.subplot(1, 4, 3)
 plt.imshow(A_cap2, cmap='gray')
 plt.xticks([])
 plt.yticks([])
-plt.xlabel('reconstructed image(d={})'.format(d2))
+plt.xlabel('d={}'.format(d2))
 
-plt.subplot(2, 2, 4)
+plt.subplot(1, 4, 4)
 plt.imshow(A_cap3, cmap='gray')
 plt.xticks([])
 plt.yticks([])
-plt.xlabel('reconstructed image(d={})'.format(d3))
+plt.xlabel('d={}'.format(d3))
 
 plt.show()
+
+# 2DPCA-based image recognition
+# get the training and test image
+all_training_imgs = []
+all_test_imgs = []
+for i in range(0, all_imgs.shape[0]):
+    if i % 10 >= 8:
+        all_test_imgs.append(
+            np.expand_dims(all_imgs[i, :, :], 0)
+        )
+    else:
+        all_training_imgs.append(
+            np.expand_dims(all_imgs[i, :, :], 0)
+        )
+all_training_imgs = np.concatenate(all_training_imgs, 0)
+all_test_imgs = np.concatenate(all_test_imgs, 0)
+
+# feature extraction by 2D-PCA
+all_training_V, U = pca_2d(all_training_imgs, 20)
+all_test_V =[]
+for i in range(0, all_test_imgs.shape[0]):
+    all_test_V.append(
+        np.expand_dims((all_test_imgs[i] @ U), 0)
+    )
+all_test_V = np.concatenate(all_test_V, 0)
+
+# test
+cls_mean = np.zeros(shape=(40, all_training_V.shape[1], all_training_V.shape[2]))
+dist = np.zeros(40)
+acc = 0
+for i in range(0, 40):
+    cls_mean[i] = np.mean(all_training_V[8*i:8*i+8, :, :], 0)
+
+for i in range(0, all_test_V.shape[0]):
+    for j in range(0, 40):
+        dist[j] = np.sum(np.linalg.norm(cls_mean[j] - all_test_V[i], axis=0, ord=2))
+    if np.argmin(dist) == (i // 2):
+        acc += 1
+    else:
+        plt.figure()
+        plt.subplot(1, 2, 1)
+        plt.imshow(all_test_imgs[i], cmap='gray')
+        plt.xticks([])
+        plt.yticks([])
+        plt.xlabel('true class:{}'.format(i//2))
+        plt.subplot(1, 2, 2)
+        plt.imshow(all_training_imgs[np.argmin(dist) * 8], cmap='gray')
+        plt.xticks([])
+        plt.yticks([])
+        plt.xlabel('predict class:{}'.format(np.argmin(dist)))
+        plt.show()
+
+print('accuracy:', acc / all_test_V.shape[0])
+
+
+
 
 print('done.')
